8d5836b9ad5953c548cd6969b14dfb10f32e920a
Дарина Даниленко <44603602+DanylenkoDaryna@users.noreply.github.com>
2022-08-10 03:19:11 +0300

    Something
    Something

--- src/index.js
+++ src/index.js
@@ -14,10 +14,7 @@ class Tabs extends React.Component {
                     <button className="tablinks" onClick="openCity(event, 'Kyiv')">Kyiv</button>
                     <button className="tablinks" onClick="openCity(event, 'Budapest')">Budapest</button>
                 </div>
-                <div id="London" className="tabcontent">
-                    <h3>London</h3>
-                    <p>London is the capital city of England.</p>
-                </div>
+               <span><p>Something</p></span>
 
                 <div id="Paris" className="tabcontent">
                     <h3>Paris</h3>
